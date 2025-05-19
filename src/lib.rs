b156309a892c3de909fc36730356d0816429a69f
Charlie Strachan <charlie@Charlie.lan>
2025-05-19 18:09:48 +0100

    First Commit

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+use rand::Rng;
+
+use std::io::{Write, stdin, stdout};
+use std::str::FromStr;
+
+fn input<T: FromStr>(prompt: &str) -> Result<T, T::Err> {
+    print!("{}", prompt);
+    stdout().flush().unwrap();
+    let mut output = String::new();
+    stdin().read_line(&mut output).unwrap();
+    output.trim().parse()
+}
+
+fn random(from: i32, to: i32) -> i32 {
+    let mut rng = rand::thread_rng();
+    rng.gen_range(from..=to)
+}
+
+fn random_float(from: f32, to: f32) -> f32 {
+    let mut rng = rand::thread_rng();
+    rng.gen_range(from..=to)
+}
